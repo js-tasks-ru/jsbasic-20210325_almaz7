58cfac424bf669d3290c3a80038c07c1ca25884a
kekaswork <kekaswork@gmail.com>
2021-04-05 14:43:11 +0300

    Calculating factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+	if ( typeof n !== 'number' || n < 0 ) {
+		return false;
+	}
+
+	let result = 1;
+
+	if ( n > 1 ) {
+
+		for ( let i = 2; i <= n; i ++ ) {
+			result *= i;
+		}
+	}
+
+	return result;
 }
