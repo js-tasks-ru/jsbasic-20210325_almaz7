e0a62ddb47206485d832160c75e157d9467ff3f1
kekaswork <kekaswork@gmail.com>
2021-04-05 17:37:28 +0300

    Antispam + Make a string shorter

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,15 @@
 function checkSpam(str) {
-  // ваш код...
+
+	if ( ! str || typeof str !== 'string' ) {
+		return false;
+	}
+
+	let result = false,
+		lowerCaseStr = str.toLowerCase();
+
+	if ( lowerCaseStr.indexOf('xxx') !== -1 || lowerCaseStr.indexOf('1xbet') !== -1 ) {
+		result = true;
+	}
+
+	return result;
 }
