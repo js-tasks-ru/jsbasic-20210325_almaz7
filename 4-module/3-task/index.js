0bc62d65065a50fec9e0cc410a3566d371d62e29
kekaswork <kekaswork@gmail.com>
2021-04-13 19:45:00 +0300

    The fourth module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,47 @@
 function highlight(table) {
-  // ваш код...
+	if ( ! table || table.length < 1 ) {
+		return false;
+	}
+
+	let rows = table.rows,
+		cells = null,
+		status = null,
+		gender = null,
+		age = null,
+		row = null;
+
+	for (let i = 1; i < rows.length; i ++) {
+		//Current row
+		row = rows[i];
+
+		//Cells
+		cells = row.cells;
+
+		// Status
+		status = cells[3];
+		// Gender
+		gender = cells[2];
+		// Age
+		age = cells[1];
+
+		if ( ! status.hasAttribute('data-available') ) { 
+			row.setAttribute('hidden', 'true');
+		} else { 
+			if ( status.dataset.available === 'true' ) { 
+				row.classList.add('available');
+			} else { 
+				row.classList.add('unavailable');
+			}
+		}
+		if ( gender.innerHTML === 'm' ) {
+			row.classList.add('male');
+		} else if ( gender.innerHTML === 'f' ) {
+			row.classList.add('female');
+		}
+		if ( Number( age.innerHTML ) < 18 ) {
+			row.style.textDecoration = 'line-through';
+		}
+	}
+
+	return true;
 }
